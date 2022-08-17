4cef8ab3562dc9a86c4a15ba1779653b74bd93ae
Anton Stecyura <grandwar39@gmail.com>
2022-08-17 14:31:53 +0300

    header responsive styles

--- src/script/app.js
+++ src/script/app.js
@@ -1,1 +1,11 @@
+const burgerOpenButton = document.querySelector('.navbar__burger-btn');
+const burgerCloseButton = document.querySelector('.burger__close-btn');
+const burger = document.querySelector('.burger');
 
+burgerOpenButton.addEventListener('click', () => {
+  burger.classList.add('active');
+});
+
+burgerCloseButton.addEventListener('click', () => {
+  burger.classList.remove('active');
+});
