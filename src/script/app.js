4ac19b8f06f64a8171323b78d163fb76c434a330
Anton Stecyura <grandwar39@gmail.com>
2022-08-28 14:10:06 +0300

    add image viewer

--- src/script/app.js
+++ src/script/app.js
@@ -9,3 +9,24 @@ burgerOpenButton.addEventListener('click', () => {
 burgerCloseButton.addEventListener('click', () => {
   burger.classList.remove('active');
 });
+
+const fullImages = [...document.querySelectorAll('.view-btn')];
+const imageViewer = document.querySelector('.image-viewer');
+const imageViewerContainer = document.querySelector('.image-viewer__img');
+const imageViewerCloseBtn = document.querySelector('.image-viewer__close-btn');
+
+fullImages.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    const src = e.target.closest('.full-image').querySelector('img').src.split('.');
+    src.pop();
+
+    imageViewerContainer.innerHTML = `<img src="${src.join('.')}.webp" alt='image'>`;
+    imageViewer.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  });
+});
+
+imageViewerCloseBtn.addEventListener('click', () => {
+  imageViewer.classList.remove('active');
+  document.body.style.overflow = '';
+});
